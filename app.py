079b5744ed2b18464f3af0d2236789da4fbff23b
fregu856 <fregu856@student.liu.se>
2017-03-22 16:00:19 -0700

    >

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/image_captioning", methods=["GET", "POST"])
+def image_captioning():
+    return render_template("image_captioning.html")
+
 if __name__ == '__main__':
     app.run()
